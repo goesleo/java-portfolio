3176201f29de08dfbb25e15efae73a2451ccd01c
Leonardo Goes <leonardotaqua20@hotmail.com>
2026-01-04 23:19:19 -0300

    chore: estrutura inicial do portfólio

---
+++ Fundamentos/ControleEstoquePadaria.java
@@ -0,0 +1,16 @@
+public class ControleEstoquePadaria {
+
+	public static void main(String[] args) {
+
+	int quantidadeFarinha = 5;
+	int quantidadeAcucar = 3;
+
+	System.out.println("Na receita vai " + quantidadeFarinha + " xicaras de farinha e " + quantidadeAcucar + " xicaras de açucar");
+		
+
+
+
+
+	}
+
+}
